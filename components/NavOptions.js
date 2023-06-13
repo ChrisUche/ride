05ff292d607c32514c74c3311ccef6acd003d960
Uche Chris <chrisuche58@yahoo.com>
2023-06-13 13:53:32 +0100

    update

---
+++ components/NavOptions.js
@@ -0,0 +1,44 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+
+const data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image: "http://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Order  food",
+        image: "http://links.papareact.com/28w",
+        screen: "EatsScreen",
+    },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList 
+    data={data}
+    keyExtractor={(item) => item.id}
+    renderItem={({ item }) => (
+        <TouchableOpacity>
+            <View>
+                <Image
+                    style={{
+                        width: 120,
+                        height: 120,
+                        resizeMode: "contain"
+                    }}
+                    source={{
+                       uri: item.image, 
+                    }} 
+                />
+            </View>
+        </TouchableOpacity>
+    )}
+    />
+  );
+};
+
+export default NavOptions
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -9,7 +9,25 @@ const initialState = {
 export const navSlice = createSlice({
     name: 'nav',
     initialState,
-    reducers: {
-        
-    }
-})
+    reducer: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        }, //when an action comes in change the state of the origin to the action
+        setDestination: (state, action) => {
+            state.origin = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.origin = action.payload;
+        },
+    },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation} = navSlice.actions;
+
+//selectors: for pulling from the data layer
+
+export const selectOrigin = (state) => state.nav.origin; // if you dont inculde "{}" in an arrow function you get a direct return
+export const selectDestination = (state) => state.nav.destination; 
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation; 
+
+export default navSlice.reducer
