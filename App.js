9ceac72334352d2c9be5d3040a2695983d246945
Uche Chris <chrisuche58@yahoo.com>
2023-06-18 19:37:45 +0100

    update

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+        <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
+      
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,8 @@
 import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { Icon } from '@rneui/base';
+
 
 const data = [
     {
@@ -20,9 +23,10 @@ const NavOptions = () => {
   return (
     <FlatList 
     data={data}
+    horizontal
     keyExtractor={(item) => item.id}
     renderItem={({ item }) => (
-        <TouchableOpacity>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             <View>
                 <Image
                     style={{
@@ -33,6 +37,13 @@ const NavOptions = () => {
                     source={{
                        uri: item.image, 
                     }} 
+                />
+                <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                <Icon 
+                    style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                    name="arrowright"
+                    color="white"
+                    type='antdesign'
                 />
             </View>
         </TouchableOpacity>
